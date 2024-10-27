b1c9dea674e614ab07b8e5085b7fe4e8b9926b9d
ikhgs <mathematiques45@gmail.com>
2024-10-27 06:45:32 +0300

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from api.definition_miriam import get_definition_api  # Importe une API spécifique
+import os
+
+app = Flask(__name__)
+
+# Page d'accueil
+@app.route('/')
+def home():
+    apis = [
+        {"name": "DÉFINITION-MIRIAM", "description": "Retrieve definitions for a given word.", "endpoint": "/definition/<word>"}
+    ]
+    return render_template('home.html', apis=apis)
+
+# Enregistrement de l'API de définition
+app.register_blueprint(get_definition_api, url_prefix="/definition")
+
+if __name__ == '__main__':
+    app.run(debug=True)
