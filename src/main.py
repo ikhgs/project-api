4328d2c9d05d9f81c71bfbab67a321c9c6f0ded5
ikhgs <mathematiques45@gmail.com>
2024-10-27 08:04:30 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -16,4 +16,4 @@ def home():
 app.register_blueprint(get_definition_api, url_prefix="/definition")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
